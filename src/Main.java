72e2986ac0985dd5f2782d73485b670591b9c35a
calebeinolf <calebeinolf@gmail.com>
2025-02-19 14:12:19 -0500

    caleb waz here

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("Commit #2");
+
+        System.out.println("caleb waz here");
+
     }
 }
